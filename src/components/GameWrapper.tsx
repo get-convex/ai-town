ec96b86a6fd5bbe778bb6c4bf57b9155e7feb801
Sujay Jayakar <sujayakar@convex.dev>
2023-08-31 18:19:20 -0400

    Tweak PlayerDetails view (#22)

--- src/components/GameWrapper.tsx
+++ src/components/GameWrapper.tsx
@@ -1,21 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import PlayerDetails from './PlayerDetails.tsx';
 import Game from './Game.tsx';
 
 import { useElementSize } from 'usehooks-ts';
 import { Id } from '../../convex/_generated/dataModel';
-import { useMutation } from 'convex/react';
-import { api } from '../../convex/_generated/api';
 
 export default function GameWrapper() {
   const [selectedPlayer, setSelectedPlayer] = useState<Id<'players'>>();
-  const talkToMe = useMutation(api.agent.talkToMe);
   const [gameWrapperRef, { width, height }] = useElementSize();
-  useEffect(() => {
-    if (selectedPlayer) {
-      void talkToMe({playerId: selectedPlayer});
-    }
-  }, [selectedPlayer]);
   return (
     <div className="mx-auto w-full max-w mt-7 grid grid-rows-[240px_1fr] lg:grid-rows-[1fr] lg:grid-cols-[1fr_auto] lg:h-[700px] max-w-[1400px] min-h-[480px] game-frame">
       {/* Game area */}
@@ -27,13 +19,7 @@ export default function GameWrapper() {
 
       {/* Right column area */}
       <div className="flex flex-col overflow-y-auto shrink-0 px-4 py-6 sm:px-6 lg:w-96 xl:pr-6 bg-brown-800 text-brown-100">
-        {selectedPlayer ? (
-          <PlayerDetails playerId={selectedPlayer} />
-        ) : (
-          <div className="h-full text-xl flex text-center items-center p-4">
-            Click on an agent on the map to see chat history.
-          </div>
-        )}
+        <PlayerDetails playerId={selectedPlayer} setSelectedPlayer={setSelectedPlayer} />
       </div>
     </div>
   );
